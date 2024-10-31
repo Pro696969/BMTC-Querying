951f2faa22b9f60697ad203086170d8b568e8a02
ad-chaos <santoshrajpurohit89@gmail.com>
2024-10-31 19:14:42 +0530

    add fancy animation

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,6 +6,9 @@ import { BotNavBar } from '@/components/navbar';
 import Link from 'next/link';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import TypeIt from 'typeit-react';
+
+
 const HomePage = () => {
   return (
     <div className="h-screen">
@@ -24,9 +27,23 @@ const HomePage = () => {
       </motion.div>
       <main className="flex flex-col items-center justify-center h-full p-4 bg-[#15151a]">
         <div className="text-center space-y-6 max-w-2xl">
-          <h1 className="text-4xl font-bold text-gray-100">
-            Find Your <span className="text-blue-500">Bus</span> Route
-          </h1>
+          <TypeIt className="text-4xl font-bold text-gray-100" getBeforeInit={(instance) => {
+            instance
+              .options({ speed: 50 })
+              .type(`Find Your <span style="color: rgb(6 182 212);">Bus</span> Route`)
+              .pause(500)
+              .move(-6)
+              .pause(700)
+              .options({ speed: 2, lifeLike: true })
+              .delete(3)
+              .pause(700)
+              .options({ speed: 2, lifeLike: true })
+              .type(`<span style="color: rgb(6 182 212);">BMTC Bus</span>`)
+              .pause(700)
+            return instance;
+          }}
+            options={{ afterComplete: (instance) => instance.destroy() }}
+          />
           <p className="text-lg text-gray-400">
             Real-time bus tracking and route information at your fingertips
           </p>
