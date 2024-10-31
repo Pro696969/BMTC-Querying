cb62948756e58e7bec55400a4c94aad5a8a1b44d
Pro696969 <krishnakumaarmc@gmail.com>
2024-10-31 19:26:37 +0530

    day1_final

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -25,13 +25,13 @@ const LoginPage = () => {
           <form onSubmit={handleSubmit} className="space-y-4 flex flex-col items-center">
             <div className="space-y-2 w-full">
               <div className="relative">
-                <Mail className="absolute left-3 top-3 h-5 w-5 text-gray-200" />
+                <Mail className="absolute left-3 top-4 h-5 w-5 text-gray-200" />
                 <input
                   type="email"
                   placeholder="Email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="bg-black w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="bg-black w-full pl-10 pr-4 py-3 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
                   required
                 />
               </div>
@@ -39,13 +39,13 @@ const LoginPage = () => {
 
             <div className="space-y-2 w-full">
               <div className="relative">
-                <Lock className="absolute left-3 top-3 h-5 w-5 text-gray-400" />
+                <Lock className="absolute left-3 top-4 h-5 w-5 text-gray-200" />
                 <input
                   type="password"
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="bg-black w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="bg-black w-full pl-10 pr-4 py-3 border rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
                   required
                 />
               </div>
