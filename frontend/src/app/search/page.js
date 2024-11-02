f5696bc5a4c9f3983628d781917a00d96fd9f06c
ad-chaos <santoshrajpurohit89@gmail.com>
2024-11-02 16:52:54 +0530

    backend integration for search

--- frontend/src/app/search/page.js
+++ frontend/src/app/search/page.js
@@ -1,28 +1,66 @@
 'use client'
-import busRoutes from "./bus_routes.json"
-import Fuse from 'fuse.js'
 import { useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import TypeIt from "typeit-react"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+
+
+const Routes = ({ routes }) => {
+  if (routes.length === 0) {
+    return <p className="text-center mt-4 text-gray-400">No routes found. Try a different search term.</p>
+  }
+  return <div className="overflow-hidden w-full max-w-4xl mx-auto rounded-xl border border-gray-700">
+    <Table className="w-full border-collapse">
+      <TableHeader>
+        <TableRow className="bg-[#0c0c0f]">
+          <TableHead className="px-4 py-2 text-left text-sm font-medium text-gray-300">Route Number</TableHead>
+          <TableHead className="px-4 py-2 text-left text-sm font-medium text-gray-300">From</TableHead>
+          <TableHead className="px-4 py-2 text-left text-sm font-medium text-gray-300">To</TableHead>
+          <TableHead className="px-4 py-2 text-left text-sm font-medium text-gray-300">Via</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {routes.map((route, index) => (
+          <TableRow
+            key={route.id}
+            className={`border-t border-gray-700 hover:bg-[#1c1c24] ${index === routes.length - 1 ? '' : 'border-b border-gray-700'
+              }`}
+          >
+            <TableCell className="px-4 py-2 text-sm font-medium text-gray-100">{route.routeNumber}</TableCell>
+            <TableCell className="px-4 py-2 text-sm text-gray-300">{route.from}</TableCell>
+            <TableCell className="px-4 py-2 text-sm text-gray-300">{route.to}</TableCell>
+            <TableCell className="px-4 py-2 text-sm text-gray-300">{route.via}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </div>
+}
 
 export default function BusSearch() {
-  const [searchResults, setSearchResults] = useState(busRoutes)
-
-  const fuse = new Fuse(busRoutes, {
-    keys: ['routeNumber', 'from', 'to', 'via'],
-    threshold: 0.3,
-  })
+  const [searchResults, setSearchResults] = useState('')
+  const [chosen, setChosen] = useState('-')
 
   const handleSearch = (event) => {
     const { value } = event.target
     if (value.length === 0) {
-      setSearchResults(busRoutes)
       return
     }
 
-    const results = fuse.search(value)
-    setSearchResults(results.map((result) => result.item))
+    fetch(`http://localhost:8000/route/${chosen}/${value}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setSearchResults(json)
+      })
   }
 
   return (
@@ -42,48 +80,31 @@ export default function BusSearch() {
               .type(`<span style="color: rgb(6 182 212);">Fuzzy Search</span>`)
               .pause(700)
             return instance;
-          }} 
+          }}
             options={{ afterComplete: (instance) => instance.destroy() }}
           />
         </h1>
-        <div className="mb-6 w-full max-w-lg mx-auto">
+        <div className="mb-6 w-full flex flex-row max-w-lg">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="w-fit rounded-xl px-4 mx-2 hover:ring-opacity-50 hover:border-sky-500 hover:border-2 transition-all duration-100 ease-in-out">{chosen}</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-black rounded-xl">
+              <DropdownMenuLabel>Category</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => setChosen("Route No")}>Route No</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setChosen("Initial")}>Initial</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setChosen("Final")}>Final</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <Input
             type="text"
             placeholder="Search by route number, from, to, or via"
             onChange={handleSearch}
-            className="w-full px-4 py-2 rounded-xl border-solid border-gray-700 bg-[#0c0c0f] text-gray-100 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-white focus:border-sky-500 focus:border-2 transition-all duration-100 ease-in-out focus:ring-opacity-50"
+            className="w-full px-4 py-2 rounded-xl border-solid border-gray-700 bg-[#0c0c0f] text-gray-100 placeholder-gray-400 focus:border-sky-500 focus:border-2 transition-all duration-100 ease-in-out focus:ring-opacity-50"
           />
         </div>
-        <div className="overflow-hidden w-full max-w-4xl mx-auto rounded-xl border border-gray-700">
-          <Table className="w-full border-collapse">
-            <TableHeader>
-              <TableRow className="bg-[#0c0c0f]">
-                <TableHead className="px-4 py-2 text-left text-sm font-medium text-gray-300">Route Number</TableHead>
-                <TableHead className="px-4 py-2 text-left text-sm font-medium text-gray-300">From</TableHead>
-                <TableHead className="px-4 py-2 text-left text-sm font-medium text-gray-300">To</TableHead>
-                <TableHead className="px-4 py-2 text-left text-sm font-medium text-gray-300">Via</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {searchResults.map((route, index) => (
-                <TableRow 
-                  key={route.id} 
-                  className={`border-t border-gray-700 hover:bg-[#1c1c24] ${
-                    index === searchResults.length - 1 ? '' : 'border-b border-gray-700'
-                  }`}
-                >
-                  <TableCell className="px-4 py-2 text-sm font-medium text-gray-100">{route.routeNumber}</TableCell>
-                  <TableCell className="px-4 py-2 text-sm text-gray-300">{route.from}</TableCell>
-                  <TableCell className="px-4 py-2 text-sm text-gray-300">{route.to}</TableCell>
-                  <TableCell className="px-4 py-2 text-sm text-gray-300">{route.via}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
-        {searchResults.length === 0 && (
-          <p className="text-center mt-4 text-gray-400">No routes found. Try a different search term.</p>
-        )}
+        <Routes routes={searchResults} />
       </div>
     </div>
   )
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,6 +1,8 @@
 import { Bus, Home, Search, Store, Headset } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { UserCredentials } from "@/app/page";
+import { useContext } from "react";
 
 const BotNavBar = () => {
   return (<div className="bg-black border-gray-200 fixed bottom-0 w-full">
@@ -33,7 +35,40 @@ const BotNavBar = () => {
   </div>);
 }
 
+const NotLoggedIn = () => {
+  return <div className="flex gap-4">
+    <Button className="bg-white font-semibold text-black hover:bg-gray-100 rounded-xl" asChild>
+      <Link href="/login">
+        Login
+      </Link>
+    </Button>
+    <Button className="bg-white font-semibold text-black hover:bg-gray-100 rounded-xl" asChild>
+      <Link href="/signup">
+        Sign Up
+      </Link>
+    </Button>
+  </div>
+}
+
+const LoggedIn = ({username}) => {
+  return <div className="flex gap-4">
+    <div className="bg-white p-1.5 font-semibold text-black rounded-xl">
+      {username}
+    </div>
+    <div className="bg-white p-1.5 font-semibold text-black rounded-xl">
+      Profile
+    </div>
+  </div>
+}
+
 const TopNavBar = () => {
+  const { username } = useContext(UserCredentials);
+  let loginItems;
+  if(username) {
+    loginItems = <LoggedIn username={username}/>
+  } else {
+    loginItems = <NotLoggedIn />
+  }
   return (
     <nav className="bg-black text-white p-4 flex justify-between items-center shadow-md">
       <Link href="/">
@@ -42,18 +77,7 @@ const TopNavBar = () => {
           <span className="text-xl font-bold">BMTC Query</span>
         </div>
       </Link>
-      <div className="flex gap-4">
-        <Button className="bg-white font-semibold text-black hover:bg-gray-100 rounded-xl" asChild>
-          <Link href="/login">
-          Login
-          </Link>
-        </Button>
-        <Button className="bg-white font-semibold text-black hover:bg-gray-100 rounded-xl" asChild>
-          <Link href="/signup">
-            Sign Up
-          </Link>
-        </Button>
-      </div>
+      {loginItems}
     </nav>);
 }
 
