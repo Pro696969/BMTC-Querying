ef50e778dec936673aaf3d16fc39b40102330531
Pro696969 <krishnakumaarmc@gmail.com>
2024-11-12 20:57:13 +0530

    bdate trigger

--- backend/users.sql
+++ backend/users.sql
@@ -12,4 +12,16 @@ DELIMITER ;
 
 SELECT username, age_calc(bdate) as age from users;
 
+DELIMITER //
+CREATE TRIGGER validate_birthdate 
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    IF YEAR(NEW.bdate) > 2024 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Birth year cannot be greater than 2024';
+    END IF;
+END //
+DELIMITER ;
+
 DELETE FROM users WHERE username=''
